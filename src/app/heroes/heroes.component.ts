b78a3cdc1bc5675325ee0fbb610b5431492044e9
pavladan <44526604+pavladan@users.noreply.github.com>
2019-04-29 22:57:08 +0300

    complite 5 steps

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero'
+import {HeroService} from '../hero.service'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  heroes : Hero[];
+  selectedHero:Hero;
+  
+  getHeroes():void{
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
+  }
+  constructor(private heroService:HeroService) {
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
